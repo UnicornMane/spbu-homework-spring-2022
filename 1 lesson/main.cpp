191511c344f235b9ecff39f7feac36b4efce477d
Alexandr Isaenkov <isaenkovcc@mail.ru>
2023-03-04 22:52:03 +0300

    uploading my new homework

---
+++ 1 lesson/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <functional>
+#include <type_traits>
+#include <thread>
+using namespace std;
+
+mutex mtx;
+
+template <typename T, typename... Ts>
+class logger
+{
+private:
+    function<T(Ts...)> f;
+
+public:
+    logger(function<T(Ts...)> f)
+    {
+        this->f = f;
+    }
+
+    void operator() (Ts... args) requires (is_same_v<T, void>)
+            {
+                    while(!mtx.try_lock()) {}
+                    {int dummy[sizeof...(Ts)] = {(std::cout << args << "; ", 0)...};}
+                    cout << endl;
+                    f(args...);
+                    cout << "thread's ID:\t" << this_thread::get_id() << endl;
+                    mtx.unlock();
+            }
+
+    T operator() (Ts... args)
+    {
+        while(!mtx.try_lock()) {}
+        {int dummy[sizeof...(Ts)] = {(std::cout << args << "; ", 0)...};}
+        cout << endl;
+        cout << "thread's ID:\t" << this_thread::get_id() << endl;
+        mtx.unlock();
+        return f(args...);
+    }
+};
+
+
+double fs(long long t, double k)
+{
+    return t/k;
+}
+
+void v_fs(long long t, double k)
+{
+    cout << t/k << endl;
+}
+
+int main()
+{
+    long long t = 6;
+    double k = 0.1;
+    logger <double, long long, double> F(fs);
+    thread th1([&]()
+               {
+                   cout << F(t, k) << endl;
+               });
+
+    logger <void, long long, double> v_F(v_fs);
+    thread th2([&]()
+               {
+                   v_F(t, k);
+               });
+
+    th1.join();
+    th2.join();
+}
